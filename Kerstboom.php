07e1abfd0dc8b6aafcc60012c73c4f30457367f9
mouaadBenAllal <mouaad_ben_allal@hotmail.com>
2016-02-09 17:12:52 +0100

    initial commit

---
+++ Kerstboom.php
@@ -0,0 +1,16 @@
+<html>
+
+<center>
+<?php
+$a=50;
+    for($i=1; $i<=$a; $i++){        
+        for($b=1; $b<=$i; $b++){
+                    echo "#";
+        }
+        
+        echo "<br />";
+    }
+
+?>
+</center>
+</html>
