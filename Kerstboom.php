4d1479fb1559c1f8e87f18a5feca8bb741069fc9
mouaadBenAllal <mouaad_ben_allal@hotmail.com>
2016-02-16 14:41:52 +0100

    initial commit

--- Kerstboom.php
+++ Kerstboom.php
@@ -6,11 +6,9 @@ $a=50;
     for($i=1; $i<=$a; $i++){        
         for($b=1; $b<=$i; $b++){
                     echo "#";
-        }
-        
+        }        
         echo "<br />";
     }
-
 ?>
 </center>
 </html>
--- zwemclubs.php
+++ zwemclubs.php
@@ -1,9 +1,15 @@
 <?php
-	$alle["leeftijd"] = 12;
-	$alle["naam"] = "Jan";
-	$alle["telefoon"] = "0612994354";
-
-	foreach ($alle as $label => $waarde) {
-		echo "de ".$label." is: ".$waarde."<br>";
-	}
+	$zwemclub["De spartelkuikens"] = "25";
+	$zwemclub["De waterbuffels"] = "32";
+	$zwemclub["Plonsmderin"] = "11";
+    $zwemclub["Bommetje"] = "23";
+    echo "<table border=2 style=width: 100% >";
+	foreach ($zwemclub as $label => $waarde) {
+		echo "<tr><td>Club" . "  " .$label." </td> <td>".$waarde."</td><td>";
+        for($i=1; $waarde/5 >= $i; $i++){
+            echo "<img src= zwemmer.jpg>";
+        }
+	echo "</td></tr>";
+    }
+    echo "</table>";
 ?>
