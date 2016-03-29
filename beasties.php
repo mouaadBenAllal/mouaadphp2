452752c1abced3997c709f5c375614635952f665
mouaadBenAllal <mouaad_ben_allal@hotmail.com>
2016-03-29 19:22:22 +0200

    initial commit

--- beasties.php
+++ beasties.php
@@ -5,39 +5,25 @@
 
 
 <h1> Kies uw dier uit.</h1>
-<form action="" method="GET">
-    <select name="KiesDier">
-        <option value="Kat">Kat</option>
-        <option value="Hond">Hond</option>
-        <option value="Egel">Egel</option>
-        <option value="Vogel">Vogel</option>
-        <option value="Muis">Muis</option>
-        
-    </select>
-    <input type="submit" name="submit" value="Verzend"/>
+
+<form action="#" method="POST">
+<select name="Dier[]" multiple> // Initializing Name With An Array
+        <option value='<img src="/BeastiesImg/Kat.png" width="200px" height= "200px">'>Kat</option> 
+        <option value='<img src="/BeastiesImg/Hond.jpg" width="200px" height= "200px">'>Hond</option>
+        <option value='<img src="/BeastiesImg/Egel.jpg" width="200px" height= "200px">'>Egel</option>
+        <option value='<img src="/BeastiesImg/Vogel.jpg" width="200px" height= "200px">'>Vogel</option>
+        <option value='<img src="/BeastiesImg/Muis.jpg" width="250px" height= "150px">'>Muis</option>
+</select>
+<input type="submit" name="submit" value="Submit" />
 </form>
 
 
 <?php
-if(isset($_GET["KiesDier"])){
-    $KiesDier = $_GET["KiesDier"];
-    switch ($KiesDier) {
-        case "Kat":
-             echo '<img src="/BeastiesImg/Kat.png" width="200px" height= "200px">'; 
-            break;
-        case "Hond":
-           echo '<img src="/BeastiesImg/Hond.jpg" width="200px" height= "200px">'; 
-            break;
-        case "Egel":
-            echo '<img src="/BeastiesImg/Egel.jpg" width="200px" height= "200px">'; 
-            break;
-        case "Vogel":
-            echo '<img src="/BeastiesImg/Vogel.jpg" width="200px" height= "200px">'; 
-            break;
-        case "Muis":
-           echo '<img src="/BeastiesImg/Muis.jpg" width="250px" height= "150px">'; 
-            break;
-    }
+if(isset($_POST['submit'])){
+    foreach ($_POST['Dier'] as $select)
+    {
+    echo "$select"; //echo "<br>";// Displaying Selected Value
+  }
 }
 ?>
 
