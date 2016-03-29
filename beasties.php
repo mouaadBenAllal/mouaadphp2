0ab279e68073387e1024c44734a394ae2820c8da
mouaadBenAllal <mouaad_ben_allal@hotmail.com>
2016-03-29 19:23:49 +0200

    initial commit

--- beasties.php
+++ beasties.php
@@ -7,7 +7,7 @@
 <h1> Kies uw dier uit.</h1>
 
 <form action="#" method="POST">
-<select name="Dier[]" multiple> // Initializing Name With An Array
+<select name="Dier[]" multiple>
         <option value='<img src="/BeastiesImg/Kat.png" width="200px" height= "200px">'>Kat</option> 
         <option value='<img src="/BeastiesImg/Hond.jpg" width="200px" height= "200px">'>Hond</option>
         <option value='<img src="/BeastiesImg/Egel.jpg" width="200px" height= "200px">'>Egel</option>
@@ -22,7 +22,7 @@
 if(isset($_POST['submit'])){
     foreach ($_POST['Dier'] as $select)
     {
-    echo "$select"; //echo "<br>";// Displaying Selected Value
+    echo "$select";
   }
 }
 ?>
