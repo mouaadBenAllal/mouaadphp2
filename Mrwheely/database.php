0ab13ed6afeafd91c90d3b2baf3a06858860b4c8
mouaadBenAllal <mouaad_ben_allal@hotmail.com>
2016-04-27 14:38:43 +0200

    Update database.php

--- Mrwheely/database.php
+++ Mrwheely/database.php
@@ -1,10 +1,15 @@
 <?php
-    $host = "127.0.0.1";
-    $user = "lau2000";                     //Your Cloud 9 username
-    $pass = "";                                  //Remember, there is NO password by default!
-    $db = "autos";                                  //Your database name you want to connect to
-    $port = 3306;                                //The port #. It is always 3306
+class dbconn {
+   private $host = "127.0.0.1";
+   private $user = "lau2000";           //Your Cloud 9 username
+   private $pass = "";                  //Remember, there is NO password by default!
+   private $port = 3306;                //The port #. It is always 3306
     
-    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+public function getDb() {
+    $connection = mysqli_connect($this->host, $this->user, $this->pass, "", $this->port) or die(mysql_error());
+    return $connection;
+ }
+
+}
 ?>
 
